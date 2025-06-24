088a617b1b8acc454a0b2802dd0c90f7adb5690e
jeyaram1023 <jeyaram.reddy.ece@gmail.com>
2025-06-24 21:22:29 +0530

    Create supabase_config.js

---
+++ supabase_config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://rnjvqxdrvplgilqzwnpl.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuanZxeGRydnBsZ2lscXp3bnBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5NDU4NjYsImV4cCI6MjA2NDUyMTg2Nn0.IOAxp8ULZgccX8hKtlDQzwdrW7xp1CcXVSdJ59UEruA';
